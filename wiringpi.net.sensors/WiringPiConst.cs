bf3de999c1b69c32ffd0d7f8d7f509c08f0244c9
mstroh76 <m.stroh76@gmail.com>
2020-06-26 12:48:11 +0200

    Update WiringPiConst.cs

--- wiringpi.net.sensors/WiringPiConst.cs
+++ wiringpi.net.sensors/WiringPiConst.cs
@@ -8,6 +8,8 @@
 		public const int Low = 0;
 		public const int High = 1;
 
+		public const int PudOff = 0;
+		public const int PudDown = 1;
 		public const int PudUp = 2;
 
 		public const int IntEdgeBoth = 3;
